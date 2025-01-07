4696c026ad100d714f74b7f3614908f430778dd9
yvonnkr <yvonnkr86@gmail.com>
2025-01-07 23:04:25 +0000

    Initial commit

---
+++ src/main/java/com/yvolabs/linklytics/LinklyticsApp.java
@@ -0,0 +1,13 @@
+package com.yvolabs.linklytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinklyticsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinklyticsApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/yvolabs/linklytics/LinklyticsAppTests.java
@@ -0,0 +1,13 @@
+package com.yvolabs.linklytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinklyticsAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
